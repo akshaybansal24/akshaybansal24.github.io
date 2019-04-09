119160a9ea9a2c943501d6e268671565bb898e7d
akshaybansal24 <akshay.bansal.2017@gmail.com>
2019-04-09 04:13:05 -0400

    Changes in Functionality

--- js/quiz/quizController.js
+++ js/quiz/quizController.js
@@ -1,16 +1,19 @@
 app.controller('quizController',['$scope','$location','$window','quizFactory',function($scope,$location,$window,quizFactory,){
 	$scope.lisOfTopic=[];
 	$scope.selecetdTopics = [];
-	$scope.check = "Welcome";
+	$scope.userType="";
+	$scope.showTopics = true;
+	$scope.hoverMsgForTopicPanel = "Select an age group first";
+	$scope.disableStatQuizBtn = true;
 
 	$scope.getListOfTopic = function(){
 		quizFactory.getQuizTopics().then(
 			function(response){
 
-				
-				console.log(JSON.stringify(response.data.trivia_categories));
+				console.log($scope.showTopics);
+				//console.log(JSON.stringify(response.data.trivia_categories));
 				for(tpc in response.data.trivia_categories){
-					console.log(JSON.stringify(response.data.trivia_categories[tpc]));
+					//console.log(JSON.stringify(response.data.trivia_categories[tpc]));
 					var topic=new Object();
 				topic.id="";
 				topic.name="";
@@ -18,20 +21,68 @@ app.controller('quizController',['$scope','$location','$window','quizFactory',fu
 					topic.id = response.data.trivia_categories[tpc].id;
 					topic.name = response.data.trivia_categories[tpc].name;
 					topic.isSelected=false;
-					console.log(JSON.stringify(topic.id));
+					//console.log(JSON.stringify(topic.id));
 					$scope.lisOfTopic.push(topic);
 				}
 				//$scope.lisOfTopic = response.data.trivia_categories;
-				console.log(JSON.stringify($scope.lisOfTopic));
+				//console.log(JSON.stringify($scope.lisOfTopic));
 			},function(error){
-				console.log(error);
+				//console.log(error);
 			});
 	}
 
 	$scope.startQuiz=function(){
-		console.log($scope.lisOfTopic);
+		for(tpc in $scope.lisOfTopic){
+			if($scope.lisOfTopic[tpc].isSelected == true){
+				$scope.selecetdTopics.push($scope.lisOfTopic[tpc]);
+			}
+			//console.log($scope.selecetdTopics);
+			//console.log($scope.userType);
+		}
 		var path = "quiz.html";
  		window.location.href = path;
 	}
+
+	$scope.startQuiz1=function(){
+			//console.log($scope.selecetdTopics);
+			//console.log($scope.userType);
+	}
+
+	$scope.$watch('userType',function(newValue,oldValue,scope){
+		console.log(newValue);
+		console.log(oldValue);
+		if(newValue != undefined && newValue!=null && newValue!=''){
+			console.log("here");
+			$scope.showTopics = false;
+			$scope.hoverMsgForTopicPanel = "";
+			$scope.disableStatQuizBtn = true;
+		}
+		else{
+			$scope.showTopics = true;
+			$scope.hoverMsgForTopicPanel = "Select an age group first";
+			$scope.disableStatQuizBtn = true;
+		}
+		if(newValue != oldValue){
+			$scope.disableStatQuizBtn = true;
+			$scope.selecetdTopics=[];
+			for(tpc in $scope.lisOfTopic){
+				$scope.lisOfTopic[tpc].isSelected = false;
+			}
+		}
+	});
+
+	$scope.$watch('lisOfTopic',function(newValue,oldValue,scope){
+		var flag = 1;
+		for(tpc in $scope.lisOfTopic){
+			if($scope.lisOfTopic[tpc].isSelected==true){
+				$scope.disableStatQuizBtn = false;
+				flag=0;
+			}
+		}
+		if(flag==1){
+			$scope.disableStatQuizBtn = true;
+		}
+		console.log("Val :" + $scope.disableStatQuizBtn);
+	},true);
 
 }]);
